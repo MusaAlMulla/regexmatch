a2df706ca38d4c56904dd9f00a79fc5189d699b2
MusaAlMulla <musaraju786@gmail.com>
2016-09-15 08:22:46 +0800

    Musa First regex

--- RegexMatch/src/regexmatch/RegexMatch.java
+++ RegexMatch/src/regexmatch/RegexMatch.java
@@ -5,9 +5,17 @@ public class RegexMatch {
     public static boolean isTrue(String str){
         return str.matches("[tT]true");
     }
-    
-    public static void main(String[] args) {
-        System.out.println(isTrue("false"));
+    public static boolean containsFalse(String str){
+        return str.matches(".*false.*");
     }
+    
+    
+    public static void main(String[]args)
+    {
+        System.out.println(isTrue("True"));
+        System.out.println(containsFalse("howareyou"));
+    }
+    
+  
     
 }
