28835c8eaf23fb708d2864bfb7c5ad4144570428
MusaAlMulla <musaraju786@gmail.com>
2016-09-15 08:35:33 +0800

    Read a File

--- RegexMatch/src/regexmatch/RegexMatch.java
+++ RegexMatch/src/regexmatch/RegexMatch.java
@@ -1,5 +1,9 @@
 package regexmatch;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class RegexMatch {
     
     public static boolean isTrue(String str){
@@ -10,10 +14,15 @@ public class RegexMatch {
     }
     
     
-    public static void main(String[]args)
+    public static void main(String[]args) throws FileNotFoundException
     {
         System.out.println(isTrue("True"));
         System.out.println(containsFalse("howareyou"));
+        Scanner sc= new Scanner (new FileReader("input.txt"));
+        
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
     }
     
   
